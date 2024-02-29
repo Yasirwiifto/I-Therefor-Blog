c44eefb1f68b00e84a8fc9f3f40e05539e17ab4d
Yasir Mohamed <Yasirwiifto@gmail.com>
2024-02-29 12:45:32 +0000

    removed everything

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://uo0xnp6c3nv:csqqijkYbuJ5@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/affix_icy_scorn_882126")
-os.environ.setdefault("SECRET_KEY", "%$%£$££$@%^$*((&^%$$£$!$±!$£^^%**(&*&%$££)))")
