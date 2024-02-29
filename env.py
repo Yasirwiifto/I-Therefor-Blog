fc66a2813a833f7eba6355b11e20287b33e3127d
Yasir Mohamed <Yasirwiifto@gmail.com>
2024-02-29 11:49:45 +0000

    added env.py

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://uo0xnp6c3nv:csqqijkYbuJ5@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/affix_icy_scorn_882126")
+os.environ.setdefault("SECRET_KEY", "Lambarafar767.ab.abc.abcd.abcde")
